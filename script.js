be8eaa0db1ed92ccf2d2736097c0488e3bac83ae
vijaykumar8899 <76232939+vijaykumar8899@users.noreply.github.com>
2023-12-29 10:34:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+
+
+
+  window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    const scrolled = window.scrollY > 500;
+  
+    if (scrolled) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
